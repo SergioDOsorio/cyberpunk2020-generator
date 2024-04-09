1b15bb942a81d071e32e6371846a6d0cb20764cf
Sergio Osorio <sdosoriogonzalez19@outlook.es>
2024-04-09 10:27:36 -0500

    First particles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './App.css'
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import NpcGenerator from './pages/NpcGenerator.jsx';
+
 
 const router = createHashRouter([
   {
--- src/pages/NpcGenerator.jsx
+++ src/pages/NpcGenerator.jsx
@@ -1,9 +1,45 @@
 import '../App.css';
+import Logo from '../img/Logo.png';
 
 export default function NpcGenerator() {
     return (
-        <div>
-            <p>Hola</p>
-        </div >
+        <>
+            <div className='main-container'>
+                <div className='container-menu'>
+                    <img src={Logo} alt="Javascript logo" className='logo' />
+                    <div>
+                        <h2>Number of NPCs</h2>
+                        <select name="" id="">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                        </select>
+                    </div>
+                    <div>
+                        <h2>Threat level</h2>
+                        <select name="" id="">
+                            <option value="">Inoffensive</option>
+                            <option value="">Regular</option>
+                            <option value="">Problematic</option>
+                            <option value="">Threatening</option>
+                        </select>
+                    </div>
+                    <div className='container-btn'> 
+                        <button>Generate</button>
+                    </div>
+
+
+                </div>
+                <div className='container-result'>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </ >
     )
 }
